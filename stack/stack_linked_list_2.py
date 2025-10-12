7f89eba115f286a02fdfa9902a385075eed9a85c
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2025-10-12 14:37:18 +0530

    stack using linked list completed

--- stack/stack_linked_list_2.py
+++ stack/stack_linked_list_2.py
@@ -5,14 +5,68 @@ class Node:
 
 class Stack:
     def __init__(self):
-        self.start = None
+        self.head = None
         self.size = 0
 
     def push(self, value):
         new_node = Node(value)
-        if self.start == None:
-            self.start = new_node
-            new_node.next = None
-        new_node.next = self.start
-        self.start = new_node
-        
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def display(self):
+        if self.head == None:
+            print("Stack is empty")
+            return
+        current = self.head
+        while current:
+            print(current.data, end = " -> ")
+            current = current.next
+        print()
+
+    def pop(self):
+        if self.head == None:
+            print("Stack is empty")
+            return
+        self.head = self.head.next
+        self.size -= 1
+
+    def stack_size(self):
+        print( "Size of stack is: ", self.size)
+        return self.size
+    
+    def is_empty(self):
+        if self.head == None:
+            print("Stack is empty")
+            return True
+        print("Stack is not empty")
+        return False
+    
+    def peek(self):
+        if self.head == None:
+            print("Stack is empty")
+            return None
+        print("Top element is: ", self.head.data)
+        return self.head.data
+    
+s = Stack()
+s.push(10)
+s.peek()
+s.push(20)
+s.peek()
+s.push(30)
+s.peek()
+s.display()
+s.stack_size()
+s.is_empty()
+s.pop()
+s.display()
+s.pop()
+s.peek()
+s.display()
+s.pop()
+s.display()
+s.stack_size()
+s.is_empty()
+s.peek()
