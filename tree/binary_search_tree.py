a73a35a8d9d268917142452be926375899df5a0c
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2025-10-25 17:39:15 +0530

    Started binary search tree

---
+++ tree/binary_search_tree.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, item = None, right = None, left = None):
+        self.item = item
+        self.right = right
+        self.left = left
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    
