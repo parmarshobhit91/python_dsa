207b7feeecfd384e58f0c2e95a125d28305c8537
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2025-11-05 17:55:33 +0530

    tree methods

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -59,12 +59,73 @@ class BST:
             result.append(root.item)
             self.ddisplay(root.right, result)
 
+    # def display(self, node):
+    #     # Pre-order traversal to display the tree
+    #     result = []
+    #     self.ddisplay(self.root, result)
+    #     return result
+    
+    def minimum(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current.item
+
+    def maximum(self, node):
+        current = node
+        while current.right is not None:
+            current = current.right
+        return current.item
+    
+    def search(self, node, key):
+        # Base Cases: root is null or key is present at root
+        if node is None or node.item == key:
+            return node
+
+        # Key is greater than root's key
+        if key > node.item:
+            return self.search(node.right, key)
+
+        # Key is smaller than root's key
+        return self.search(node.left, key)
+    
+    def delete(self, node, data):
+        if node is None:
+            return node
+        
+        if data < node.item:
+            node.left = self.delete(node.left, data)
+
+        elif data > node.item:
+            node.right = self.delete(node.right, data)
+        
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            node.item = self.minimum(node.right)
+            node.right = self.delete(node.right, node.item)
+        return node
 
 new_bst = BST()
 
-new_bst.insert(10)
-new_bst.insert(5)
-new_bst.insert(15)
+new_bst.insert(2)
 new_bst.insert(3)
+new_bst.insert(10)
+new_bst.insert(15)
+new_bst.insert(6)
+new_bst.insert(1)
 
 print(new_bst.display(new_bst.root))  # [3, 5, 10, 15]
+
+# print(new_bst.minimum(new_bst.root))  # 3
+# print(new_bst.maximum(new_bst.root))  # 15
+
+# print(new_bst.search(new_bst.root, 88))  # 5
+# print(new_bst.search(new_bst.root, 15))  # 5
+# print(new_bst.search(new_bst.root, 3))  # 5
+
+
+new_bst.delete(new_bst.root, 2)
+print(new_bst.display(new_bst.root))
