6d4bfed38c11aec5861eee4bde73dcc5ee4f21e4
parmarshobhit91 <109821693+parmarshobhit91@users.noreply.github.com>
2025-11-18 17:22:29 +0530

    insertion sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,19 @@
+# Insertion Sort Implementation
+# 50, 20, 37, 91, 64, 18
+
+def insertion_sort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # Move elements of arr[0..i-1], that are greater than key,
+        # to one position ahead of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+arr = [50, 20, 37, 91, 64, 18]
+print(insertion_sort(arr))
